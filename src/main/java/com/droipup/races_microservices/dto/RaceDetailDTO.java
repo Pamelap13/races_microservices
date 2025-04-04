9813b07e0dd4cf51c245586766bae347c002e060
Pamela <pamela@MacBook-Pro-de-Pamela.local>
2025-04-04 15:29:16 -0500

    test deploy

--- src/main/java/com/droipup/races_microservices/dto/RaceDetailDTO.java
+++ src/main/java/com/droipup/races_microservices/dto/RaceDetailDTO.java
@@ -2,6 +2,5 @@ package com.droipup.races_microservices.dto;
 
 public record RaceDetailDTO(String name, String description,
                             String organizer, String eventLink, Double totalDistance,
-                            Double totalElevation, String district, String province, String department
-) {
+                            Double totalElevation, String district, String province, String department) {
 }
